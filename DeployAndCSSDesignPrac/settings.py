c6c10611a9ee91829eaf0fe2f471781d5178f4ef
Tendency-Sweet-Party <1104adad@gmail.com>
2020-03-13 05:29:51 +0900

    html_base_2 DB_setting_migrate

--- DeployAndCSSDesignPrac/settings.py
+++ DeployAndCSSDesignPrac/settings.py
@@ -55,6 +55,9 @@ INSTALLED_APPS = [
 
 ]
 
+# setting for site
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- base_app/urls.py
+++ base_app/urls.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 app_name = 'base'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='base_app/base.html'), name='index')
+    path('', TemplateView.as_view(template_name='base_app/base.html'), name='base'),
+    path('text_base/', TemplateView.as_view(template_name='base_app/text_page_base.html'), name='index'),
 ]
