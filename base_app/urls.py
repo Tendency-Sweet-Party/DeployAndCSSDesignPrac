0b50c7340c586cfe8aba2781a8cd98239e2644e5
Tendency-Sweet-Party <1104adad@gmail.com>
2020-03-16 04:11:56 +0900

    content page common ver1

--- base_app/urls.py
+++ base_app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from base_app.views import InitialSettingView
+from base_app import views
 
 app_name = 'base'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='base_app/base.html'), name='base'),
-    path('text_base/', TemplateView.as_view(template_name='base_app/text_page_base.html'), name='index'),
+    path('text_base/', TemplateView.as_view(template_name='base_app/text_part/text_page_base.html'), name='index'),
     path('quote/', TemplateView.as_view(template_name='base_app/quote.html'), name='quote'),
-    path('title/', InitialSettingView.as_view(), name='title'),
+    path('title/', views.InitialSettingView.as_view(), name='title'),
+    path('page/<int:page_num>', views.page_create, name='content_pages'),
 ]
--- base_app/views.py
+++ base_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import FormView
 
 from base_app.forms import InitialSettingForm
@@ -8,7 +10,54 @@ class InitialSettingView(FormView):
     template_name = 'base_app/initial_setting.html'
     form_class = InitialSettingForm
 
+    def post(self, request, *args, **kwargs):
+        if request.POST.get('next', '') == 'create':
+            form = InitialSettingForm(request.POST)
+            if form.is_valid():
+                # ctx = {}
+                # セッションにデータを保存
+                initial_setting_data = {
+                    'main_character_name': form.cleaned_data['main_character_name'],
+                    'special_move': form.cleaned_data['special_move'],
+                    'job_after': form.cleaned_data['job_after'],
+                    'state_of_progress': 1,
+                }
+                request.session['initial_setting_data'] = initial_setting_data
+                # ctx['initial_setting_data'] = initial_setting_data
+                return redirect(reverse('base:content_pages', kwargs={'page_num': 1, }))
+            else:
+                return render(request, self.template_name, {'form': form})
+        else:
+            return redirect(reverse_lazy('base:title'))
+
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         ctx['setting_component'] = ctx.pop('form')
         return ctx
+
+
+def page_create(request, page_num):
+    template_name = ''
+
+    if not page_num:
+        return HttpResponseForbidden('403 Forbidden', content_type='text/html')
+    elif 'initial_setting_data' not in request.session:
+        return HttpResponseForbidden('403 Forbidden', content_type='text/html')
+    elif request.session['initial_setting_data']['state_of_progress'] < page_num:
+        return HttpResponseForbidden('403 Forbidden', content_type='text/html')
+
+    # 進行度をインクリメント
+    request.session['initial_setting_data']['state_of_progress'] = request.session['initial_setting_data'].get(
+        'state_of_progress', 0) + 1
+    # contextを作成
+    data = {
+        'main_character_name': request.session['initial_setting_data']['main_character_name'],
+        'special_move': request.session['initial_setting_data']['special_move'],
+        'job_after': request.session['initial_setting_data']['job_after'],
+        'state_of_progress': request.session['initial_setting_data']['state_of_progress'],
+    }
+    ctx = {'data': data}
+    # template先を編集
+    template_name = 'common_part_' + str(page_num) + '.html'
+
+    return render(request, 'base_app/text_part/' + template_name, ctx)
