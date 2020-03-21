f526039d3e29a2b21b051be46760da8e3256f147
Tendency-Sweet-Party <1104adad@gmail.com>
2020-03-21 20:54:09 +0900

    static pages ver1

--- base_app/urls.py
+++ base_app/urls.py
@@ -14,7 +14,9 @@ urlpatterns = [
     path('title/', views.InitialSettingView.as_view(), name='title'),
     path('page/<int:page_num>', views.page_create, name='content_pages'),
     path('bad_end_page/<str:bad_end_name>', views.bad_end_page_create, name='bad_end_pages'),
-    path('quote/', TemplateView.as_view(template_name='base_app/quote.html'), name='quote'),
     # status function
     path('status/', views.StatusView.as_view(), name='status'),
+    # others
+    path('quote/', TemplateView.as_view(template_name='base_app/quote.html'), name='quote'),
+    path('about/', TemplateView.as_view(template_name='base_app/about.html'), name='about'),
 ]
