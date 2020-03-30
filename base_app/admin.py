f0ce8c46721c577d753884df7ed663aaff3b1208
Tendency-Sweet-Party <1104adad@gmail.com>
2020-03-30 16:51:19 +0900

    system _ intensify admin functions

--- base_app/admin.py
+++ base_app/admin.py
@@ -1,14 +1,54 @@
+import pprint
+
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
 from django.contrib.sessions.models import Session
+from django.utils.safestring import mark_safe
+
+from base_app.admin_actions import export_as_csv_action
 
 
+@admin.register(Session)
 class SessionAdmin(ModelAdmin):
     def _session_data(self, obj):
-        return obj.get_decoded()
+        return mark_safe(pprint.pformat(obj.get_decoded()).replace('\n', '<br>\n'))
 
-    list_display = ['session_key', '_session_data', 'expire_date']
+    def _session_property(self, obj, property_name):
+        d = obj.get_decoded()
+        initial_setting_data = d['initial_setting_data'] if 'initial_setting_data' in d else ''
+        return initial_setting_data[property_name] if property_name in initial_setting_data else ''
 
+    def _main_character_name(self, obj):
+        return SessionAdmin._session_property(self, obj, 'main_character_name')
 
-admin.site.register(Session, SessionAdmin)
+    def _special_move(self, obj):
+        return SessionAdmin._session_property(self, obj, 'special_move')
+
+    ITEMS = (
+        'session_key',
+        '_session_data',
+        '_main_character_name',
+        '_special_move',
+        'expire_date',
+    )
+
+    DISPLAYED_ITEMS = (
+        'session_key',
+        '_main_character_name',
+        '_special_move',
+        'expire_date',
+    )
+
+    list_display = DISPLAYED_ITEMS
+    list_display_links = DISPLAYED_ITEMS
+    list_filter = (
+        'session_key',
+        'expire_date',
+    )
+    readonly_fields = ITEMS
+    exclude = ('session_data',)
+    ordering = ('-expire_date',)
+    date_hierarchy = 'expire_date'
+
+    actions = [export_as_csv_action('CSV Export', fields=ITEMS), ]
---
+++ base_app/admin_actions.py
@@ -0,0 +1,43 @@
+# quoted by https://gist.github.com/jeremyjbowers/e8d007446155c12033e6
+import csv
+from django.http import HttpResponse
+
+
+def export_as_csv_action(description="Export selected objects as CSV file", fields=None, exclude=None, header=True):
+    """
+    This function returns an export csv action
+    'fields' and 'exclude' work like in django ModelForm
+    'header' is whether or not to output the column names as the first row
+    """
+
+    def export_as_csv(modeladmin, request, queryset):
+        """
+        Generic csv export admin action.
+        based on http://djangosnippets.org/snippets/1697/
+        """
+        opts = modeladmin.model._meta
+        field_names = set([field.name for field in opts.fields])
+
+        if fields:
+            new_fields = [(x[1:len(x)] if x and x[0] == '_' else x) for x in fields]
+            fieldset = set(new_fields)
+            field_names = field_names & fieldset
+
+        elif exclude:
+            excludeset = set(exclude)
+            field_names = field_names - excludeset
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=%s.csv' % str(opts).replace('.', '_')
+
+        writer = csv.writer(response)
+
+        if header:
+            writer.writerow(list(field_names))
+        for obj in queryset:
+            writer.writerow([str(getattr(obj, field)) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = description
+    return export_as_csv
